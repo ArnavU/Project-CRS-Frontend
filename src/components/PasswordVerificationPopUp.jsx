230d2dfd47ef1e971f21dd853462a819886a1d13
RohanNikumbh27 <rohannikumbh2793@gmail.com>
2024-03-24 00:00:10 +0530

    CSS for -password to delete pdf:div

--- src/components/PasswordVerificationPopUp.jsx
+++ src/components/PasswordVerificationPopUp.jsx
@@ -1,41 +1,52 @@
-import React, { useState } from 'react'
-import useOpenPdf from '../hooks/useOpenPdf';
+import React, { useState } from "react";
+import useOpenPdf from "../hooks/useOpenPdf";
 import { RxCross1 } from "react-icons/rx";
-import useDeletePdf from '../hooks/useDeletePdf';
+import useDeletePdf from "../hooks/useDeletePdf";
 
-const PasswordVerificationPopUp = ({pdf, setDeletePdfId, setReload, reload}) => {
+const PasswordVerificationPopUp = ({
+  pdf,
+  setDeletePdfId,
+  setReload,
+  reload,
+}) => {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
 
   const handlePasswordSubmit = (e) => {
     e.preventDefault();
     setErrorMessage("");
-    if(password === "abc@123") {
+    if (password === "abc@123") {
       useDeletePdf(pdf, setReload, reload);
       setDeletePdfId("");
+    } else {
+      setErrorMessage("Incorrect password");
     }
-    else {
-      setErrorMessage("Incorrect password")
-    }
-  }
-
+  };
 
   return (
-    <div className='absolute top-0 left-0'>
-      <div><RxCross1 /></div>
-        <form onSubmit={handlePasswordSubmit} className='verification-form bg-black'>
-          <div className='text-red-600 font-bold'>Delete: <br/>{pdf.name}</div>
+    <div className="absolute top-0 left-0 delete-pdf--card backdrop-blur-xl">
+      <div className="delete-pdf--card-cross-icon">
+        <RxCross1 />
+      </div>
+      <form onSubmit={handlePasswordSubmit} className="verification-form ">
+        <div className="text-red-600 font-bold">
+          Delete: <br />
+          {pdf.name}
+        </div>
         <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Enter password"
+          type="password"
+          style={{ padding: "2px 10px", marginTop: "5px" }}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter password"
         />
-        <p className='text-red-500'>{errorMessage}</p>
-        <button type="submit">Submit</button>
-        </form>
+        <p className="text-red-500">{errorMessage}</p>
+        <button type="submit" style={{ background: "red" }}>
+          Delete
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default PasswordVerificationPopUp
+export default PasswordVerificationPopUp;
